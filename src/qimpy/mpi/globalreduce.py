6622e40fde0b015b34754bdd57bab6a2f56f02f8
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2026-09-21 20:54:07 -0400

    Fixed zero-size behavior in globalreduce (bug since dist refactor)

--- src/qimpy/mpi/globalreduce.py
+++ src/qimpy/mpi/globalreduce.py
@@ -3,7 +3,7 @@ The functions of this module correctly handle zero-sized pieces of
 distributed tensors on certain processes, which is a frequently
 encountered and cumbersome corner case in such global reductions."""
 
-__all__ = ["sum", "prod", "min", "max", "all", "any"]
+__all__ = ["sum", "prod", "min", "max"]
 
 import torch
 import torch.distributed as dist
@@ -11,14 +11,14 @@ import torch.distributed as dist
 
 def sum(v: torch.Tensor, group: dist.ProcessGroup) -> torch.Tensor:
     """Global sum of tensor `v` distributed over `group`."""
-    result = v.sum() if v.numel() else torch.zeros(1, dtype=v.dtype, device=v.device)
+    result = v.sum() if v.numel() else torch.zeros((), dtype=v.dtype, device=v.device)
     dist.all_reduce(result, op=dist.ReduceOp.SUM, group=group)
     return result
 
 
 def prod(v: torch.Tensor, group: dist.ProcessGroup) -> torch.Tensor:
     """Global product of tensor `v` distributed over `group`."""
-    result = v.prod() if v.numel() else torch.ones(1, dtype=v.dtype, device=v.device)
+    result = v.prod() if v.numel() else torch.ones((), dtype=v.dtype, device=v.device)
     dist.all_reduce(result, op=dist.ReduceOp.PRODUCT, group=group)
     return result
 
@@ -29,7 +29,7 @@ def min(v: torch.Tensor, group: dist.ProcessGroup) -> torch.Tensor:
         result = v.min()
     else:
         info = torch.finfo if v.dtype.is_floating_point else torch.iinfo
-        result = torch.full((1,), info(v.dtype).max, dtype=v.dtype, device=v.device)
+        result = torch.full((), info(v.dtype).max, dtype=v.dtype, device=v.device)
     dist.all_reduce(result, op=dist.ReduceOp.MIN, group=group)
     return result
 
@@ -40,6 +40,6 @@ def max(v: torch.Tensor, group: dist.ProcessGroup) -> torch.Tensor:
         result = v.max()
     else:
         info = torch.finfo if v.dtype.is_floating_point else torch.iinfo
-        result = torch.full((1,), info(v.dtype).min, dtype=v.dtype, device=v.device)
+        result = torch.full((), info(v.dtype).min, dtype=v.dtype, device=v.device)
     dist.all_reduce(result, op=dist.ReduceOp.MAX, group=group)
     return result
